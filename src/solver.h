28125b05eadffebb7d627ab13fdaa1b5e17de642
Dylan Missuwe <dylan.missu@gmail.com>
2024-02-18 20:54:28 +0100

    added multithreading in the poisson solver

--- src/solver.h
+++ src/solver.h
@@ -1,15 +1,8 @@
 #ifndef SOLVER_H
 #define SOLVER_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+#include <thread>
 
-void relax(std::vector<std::vector<double>> &D, std::vector<std::vector<double>> &phi, int width, int height, double omega, double &max_update);
 void poisson_solver(std::vector<std::vector<double>> &D, std::vector<std::vector<double>> &phi, int width, int height, int max_iterations, double convergence_threshold);
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif // SOLVER_H
