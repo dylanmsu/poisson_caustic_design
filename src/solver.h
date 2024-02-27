7ba834bd199d74963e65c07e974a814066fe1012
Dylan Missuwe <dylan.missu@gmail.com>
2024-02-27 15:59:10 +0100

    added more fault tolerance and opdated readme

--- src/solver.h
+++ src/solver.h
@@ -2,6 +2,7 @@
 #define SOLVER_H
 
 #include <thread>
+#include <cmath>
 
 void poisson_solver(std::vector<std::vector<double>> &D, std::vector<std::vector<double>> &phi, int width, int height, int max_iterations, double convergence_threshold, int max_threads);
 
